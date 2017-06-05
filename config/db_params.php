89ba9098a252eb7175d086494f62d70c61b853b2
evrekhman <evrekhman@gmail.com>
2017-06-05 11:31:53 +0600

    Update db_params.php

--- config/db_params.php
+++ config/db_params.php
@@ -2,7 +2,7 @@
 
 return array(
 	'host' => 'localhost',
-    'dbname' => 'evrekhgk_med',
-    'user' => 'evrekhgk_med',
-    'password' => 'visacard2', 
+    'dbname' => '',
+    'user' => '',
+    'password' => '', 
 );
